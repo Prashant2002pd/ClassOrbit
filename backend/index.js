7038d0c502212320a20727b06a0a875eb567394f
Prashant2002pd <92350732+Prashant2002pd@users.noreply.github.com>
2024-04-26 21:00:41 +0530

    some updates

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,12 @@ const cors =require('cors')
 const app=express();
 app.use(cors())
 app.use(express.json())
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+    next();
+  })
 app.get(('/'), (req,res)=>{
     Students.find()
         .then((data) =>{
